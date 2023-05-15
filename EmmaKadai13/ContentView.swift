c591487a55fdba5e96dba626565a2b66e8ffd863
emma <emmatone7@gmail.com>
2023-05-15 11:46:57 +0900

    チェックマーク部分をプロパティ化し、isChecked = falseの場合は.hidden()とするよう変更。

--- EmmaKadai13/ContentView.swift
+++ EmmaKadai13/ContentView.swift
@@ -8,29 +8,37 @@ import SwiftUI
 
 struct ContentView: View {
     @ObservedObject private var fruits = Fruits()
+    let checkMark: some View = Image(systemName: "checkmark").foregroundColor(.orange)
 
     var body: some View {
         NavigationStack {
-            Text("4つの項目がチェックマーク付きで\n表示されるアプリ")
+            Text("4つの項目がチェックマーク付きで表示されるアプリ")
                 .font(.headline)
                 .padding(.bottom, 15)
+                .padding(.horizontal, 60)
                 .frame(maxWidth: .infinity)
                 .background(.mint.opacity(0.5))
 
             List(fruits.fruitsData) { fruit in
-                Label(fruit.name, systemImage: fruit.isChecked ?  "checkmark" : "")
+                Label{
+                    Text(fruit.name)
+                } icon: {
+                    if fruit.isChecked {
+                        checkMark
+                    } else {
+                        checkMark
+                            .hidden()
+                    }
+                }
             }
             .padding(.vertical, 10)
-            .tint(.orange)
             .listStyle(.inset)
-
             .navigationTitle("課題13")
             .navigationBarTitleDisplayMode(.inline)
             .ignoresSafeArea(edges: [.leading, .trailing] )
         }
     }
 }
-
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
